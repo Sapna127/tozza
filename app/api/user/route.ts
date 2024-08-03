99f9a35e23dc916c957f88f4b200bd770fe96a70
Sapna Kul <kulsapna2210002@gmail.com>
2024-08-03 19:07:14 +0530

    init commit

---
+++ app/api/user/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req:NextRequest){
+    const body = await req.json();
+
+   console.log(body);
+   return Response.json({
+    message:"user logged in",
+   })
+}
