03e241e43b6b7a25e0b9ab9bb93a7dadcd5e97d3
Sapna Kul <kulsapna2210002@gmail.com>
2024-08-10 13:31:32 +0530

    schema creation done

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
 import axios from "axios";
-
-async function getUserDetails() {
-  const response = await axios.get("http://localhost:3000/api/user")
-	return response.data;
-}
+import TodoItem from "@/components/TodoItem";
+// async function getUserDetails() {
+//   const response = await axios.get("http://localhost:3000/api/user")
+// 	return response.data;
+// }
 
 export default async function Home() {
-  const userData = await getUserDetails();
+  // const userData = await getUserDetails();
 
   return (
-    <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userData?.name}
-                </div>
+    // <div className="flex flex-col justify-center h-screen">
+    //     <div className="flex justify-center">
+    //         <div className="border p-8 rounded">
+    //             <div>
+    //                 Name: {userData?.name}
+    //             </div>
                 
-                {userData?.email}
-            </div>
-        </div>
-    </div>
+    //             {userData?.email}
+    //         </div>
+    //     </div>
+    // </div>
+    <>
+
+      <TodoItem/>
+    </>
   );
 }
