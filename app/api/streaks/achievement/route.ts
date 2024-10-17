4260fb8c2843f3e145b19904956bcc3eb1616873
Sapna Kul <kulsapna2210002@gmail.com>
2024-10-17 19:45:23 +0530

    fix: Achievements route fix

--- app/api/streaks/achievement/route.ts
+++ app/api/streaks/achievement/route.ts
@@ -1,9 +1,15 @@
+import { NextResponse } from 'next/server';
 import prisma from '../../../../lib/prisma'
 
-export default async function handler(req:any, res:any) {
-  const { userId } = req.query;
-  const achievements = await prisma.achievement.findMany({
-    where: { userId },
-  });
-  res.json(achievements);
+export default async function GET(req:any, res:any) {
+  try{
+    const { userId } = req.query;
+    const achievements = await prisma.achievement.findMany({
+      where: { userId },
+    });
+    res.json(achievements);
+  }catch(error){
+    return NextResponse.json({ error: 'Failed to fetch achievements..' }, { status: 500 });
+  }
+  
 }
