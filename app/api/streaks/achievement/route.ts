04febadfa9e3e50736a2f89d1356747f28de8e01
Sapna Kul <kulsapna2210002@gmail.com>
2024-10-17 20:18:32 +0530

    route fix

--- app/api/streaks/achievement/route.ts
+++ app/api/streaks/achievement/route.ts
@@ -1,15 +1,21 @@
 import { NextResponse } from 'next/server';
-import prisma from '../../../../lib/prisma'
+import prisma from '../../../../lib/prisma';
 
-export const GET = (req:any, res:any) {
-  try{
-    const { userId } = req.query;
+export const GET = async (req: Request) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Missing userId parameter' }, { status: 400 });
+    }
+
     const achievements = await prisma.achievement.findMany({
       where: { userId },
     });
-    res.json(achievements);
-  }catch(error){
+
+    return NextResponse.json(achievements);
+  } catch (error) {
     return NextResponse.json({ error: 'Failed to fetch achievements..' }, { status: 500 });
   }
-  
-}
+};
